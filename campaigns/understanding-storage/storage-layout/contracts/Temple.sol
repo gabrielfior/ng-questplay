9aeebeaf42ec89353bedf1d788f6f5dc46ce8e08
Gabriel Fior <gabe@apy.vision>
2023-08-13 17:09:34 -0300

    finished understanding storage

--- campaigns/understanding-storage/storage-layout/contracts/Temple.sol
+++ campaigns/understanding-storage/storage-layout/contracts/Temple.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.19;
+import "/home/gabrielfior/code/ng-questplay/lib/forge-std/src/console.sol";
 
 contract Temple {
     uint128 public entrance;
@@ -7,10 +8,16 @@ contract Temple {
     mapping(uint8 => mapping(uint8 => address)) public gardens;
     bytes20[] public chambers;
 
+    constructor(){
+        console.log("encode", bytes32(abi.encode(0xa5E2e096FD20ad035151C3d467E5B063b40Cf175)));
+        //write(1, bytes32(abi.encode(msg.sender)));
+    }
+
     /// Write data to the contract's ith storage slot
     function write(uint256 i, bytes32 data) public {
         assembly {
             sstore(i, data)
         }
+        mainHall = msg.sender;
     }
 }
