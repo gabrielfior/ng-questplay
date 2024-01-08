dc5f70638b3d449aff33c61924dea026c5e5c726
Gabriel Fior <fior.gbm@gmail.com>
2024-01-08 19:23:12 -0300

    Download quest hello-noir

---
+++ campaigns/discovering-noir/hello-noir/test/testsuites/testNoir.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const { spawnSync } = require("child_process");
+const { expect } = require("chai");
+
+async function runNargoCheck() {
+    // Check for compile error
+    const result = spawnSync("nargo", ["check"] );
+    expect(result.status).to.equal(0, result.stderr.toString());
+
+    // Check that tests not modified
+    const mainSrc = fs.readFileSync("src/main.nr", "utf-8").replaceAll(/\s+/g," ");
+    const prefix = "mod tests; // Do not modify this first line!"
+
+    expect(mainSrc.startsWith(prefix))
+        .to.equal(true, "First line of main.nr modified!");
+}
+
+function runNargoTest(testName) {
+    const testPath = `tests::${testName}`;
+    const result = spawnSync("nargo", ["test", testPath] );
+
+    expect(result.status).to.equal(0, "\n"  + result.stderr.toString());
+}
+
+module.exports = {
+    runNargoCheck,
+    runNargoTest
+};
